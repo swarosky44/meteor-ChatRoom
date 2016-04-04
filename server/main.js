a2ec3230d454c6875f17ef6319667d6b4f64a356
swarosky44 <495177660@qq.com>
2016-04-04 14:49:57 +0800

    init

--- server/main.js
+++ server/main.js
@@ -1,7 +1,5 @@
 import { Meteor } from 'meteor/meteor'
-import '../imports/api/tasks.js'
 import '../imports/api/msgs.js'
-import '../imports/api/users.js'
 Meteor.startup(() => {
   // code to run on server at startup
 });
